b98e72dd475f202d9d4a00f662e4cb38a5850c5d
Felipe Leite <lipi4shared@hotmail.com>
2025-11-15 06:17:13 -0300

    Ajuste

--- src/app/slider3/page.tsx
+++ src/app/slider3/page.tsx
@@ -39,6 +39,9 @@ export default function Slider() {
 	const lastProduct1 = useRef<HTMLImageElement>(null)
 	const lastProduct2 = useRef<HTMLImageElement>(null)
 	const lastProduct3 = useRef<HTMLImageElement>(null)
+	const triggerScroll = useRef<HTMLDivElement>(null)
+	const potatoes = useRef<HTMLImageElement>(null)
+	const potatoes2 = useRef<HTMLImageElement>(null)
 
 	const slides = [
 		{ 
@@ -127,8 +130,8 @@ export default function Slider() {
 			opacity: 0,
 			y: 50,
 			stagger: 0.05,
-			duration: 0.3,
-			delay: 0.3
+			duration: 0.25,
+			delay: 0.25
 		})
 
 		gsap.from(details.current, {
@@ -142,7 +145,7 @@ export default function Slider() {
 			opacity: 0,
 			y: 30,
 			duration: 0.3,
-			delay: 0.7
+			delay: 0.3
 		})
 
 		gsap.from(image.current, {
@@ -153,7 +156,7 @@ export default function Slider() {
 
 		gsap.to(product.current, {
 			scale: 0.4,
-			opacity: 0,
+			opacity: 0.4,
 			transformOrigin: 'bottom'
 		})
 
@@ -170,9 +173,37 @@ export default function Slider() {
 			opacity: 1,
 			transformOrigin: 'bottom',
 			delay: 0.5,
-			duration: 0.4
+			duration: 0.2
+		})
+
+		gsap.from(potatoes.current, {
+			y: 40,
+			opacity: 0,
+			delay: 0.5
 		})
 	}
+
+	useEffect(() => {
+		gsap.to(slides[currentIndex].refs.product.current, {
+			rotate: -4,
+			scrollTrigger: {
+				trigger: triggerScroll.current,
+				start: "top 100%",
+				end: "top 50%",
+				scrub: true
+			}
+		})
+
+		gsap.to(potatoes2.current, {
+			rotate: -6,
+			scrollTrigger: {
+				trigger: triggerScroll.current,
+				start: "top 100%",
+				end: "top 50%",
+				scrub: true
+			}
+		})
+	}, [])
 
     useEffect(() => {
         if (animatingToIndex === null) return
@@ -207,79 +238,89 @@ export default function Slider() {
     }, [animatingToIndex])
 
     return (
-        <div ref={container} className="relative w-screen h-screen overflow-hidden">
-            {/* The current slide is always rendered at the last layer */}
-            <div className={`absolute inset-0 ${slides[currentIndex].color} z-10`}>
-				<div className="flex justify-between gap-32 p-24 pl-0 items-center">
-					<div className="w-60 select-none">
-						<div className="w-120">
-							<img ref={slides[currentIndex].refs.image} src={slides[currentIndex].image} alt="" className="rounded-full w-100 h-100 -translate-x-1/2 object-cover shadow-lg" />
-						</div>
-					</div>
+		<main className="flex flex-col h-[200vh] overflow-hidden">
+			<div ref={container} className="relative w-screen h-screen overflow-hidden">
+				{/* The current slide is always rendered at the last layer */}
+				<div className={`absolute inset-0 ${slides[currentIndex].color} z-10`}>
+					<img src="/images/pringles.svg" alt="Pringles" className="-rotate-10 absolute -bottom-64 -right-48 w-160 -z-10 opacity-10" />
 
-					<div className="flex flex-col gap-12 flex-1">
-						<h1 ref={slides[currentIndex].refs.title} className="font-black text-5xl">{slides[currentIndex].title}</h1>
-						<p ref={slides[currentIndex].refs.details} className="leading-relaxed text-sm w-120">{slides[currentIndex].details}</p>
-						<button ref={slides[currentIndex].refs.cta} className="uppercase p-3 bg-white rounded-md text-slate-800 font-medium w-fit">Ver na loja</button>
-					</div>
-
-					<div className="flex gap-6 items-end">
-						<img ref={slides[currentIndex].refs.product} src={slides[currentIndex].product} alt="Produto" className="w-40" />
-						<img ref={slides[currentIndex].refs.nextProduct} src={slides[currentIndex].nextProduct} alt="Próximo produto" className="w-32 -translate-y-4 cursor-pointer" onClick={() => handleNavigate('next')} />
-					</div>
-				</div>
-			</div>
-
-            {/* The animating slide is rendered on top only when the animation is in progress */}
-            {isAnimating && (
-                 <div
-                    key={animatingToIndex}
-                    className={`absolute inset-0 ${slides[animatingToIndex].color} z-20`}
-                    style={clipPathStyle}
-                >
 					<div className="flex justify-between gap-32 p-24 pl-0 items-center">
 						<div className="w-60 select-none">
 							<div className="w-120">
-								<img ref={slides[animatingToIndex].refs.image} src={slides[animatingToIndex].image} alt="" className="rounded-full w-100 h-100 -translate-x-1/2 object-cover shadow-lg" />
+								<img ref={slides[currentIndex].refs.image} src={slides[currentIndex].image} alt="" className="rounded-full w-100 h-100 -translate-x-1/2 object-cover shadow-lg" />
 							</div>
 						</div>
 
 						<div className="flex flex-col gap-12 flex-1">
-							<h1 ref={slides[animatingToIndex].refs.title} className="font-black text-5xl">{slides[animatingToIndex].title}</h1>
-							<p ref={slides[animatingToIndex].refs.details} className="leading-relaxed text-sm w-120">{slides[animatingToIndex].details}</p>
-							<button ref={slides[animatingToIndex].refs.cta} className="uppercase p-3 bg-white rounded-md text-slate-800 font-medium w-fit">Ver na loja</button>
+							<h1 ref={slides[currentIndex].refs.title} className="font-black text-5xl">{slides[currentIndex].title}</h1>
+							<p ref={slides[currentIndex].refs.details} className="leading-relaxed text-sm w-120">{slides[currentIndex].details}</p>
+							<button ref={slides[currentIndex].refs.cta} className="uppercase cursor-pointer hover:opacity-90 transition-opacity p-3 bg-white rounded-md text-slate-800 font-medium w-fit">Ver na loja</button>
 						</div>
 
-						<div className="flex gap-6 items-end">
-							<img ref={slides[animatingToIndex].refs.product} src={slides[animatingToIndex].product} alt="Produto" className="w-40" />
-							<img ref={slides[animatingToIndex].refs.nextProduct} src={slides[animatingToIndex].product} alt="Próximo produto" className="w-32" />
-							<img ref={slides[animatingToIndex].refs.lastProduct} src={slides[animatingToIndex].nextProduct} alt="Próximo produto" className="w-28 opacity-1" />
+						<div className="flex gap-6 items-end select-none w-[294px] relative">
+							<img ref={slides[currentIndex].refs.product} src={slides[currentIndex].product} alt="Produto" className="w-40" />
+							<img ref={slides[currentIndex].refs.nextProduct} src={slides[currentIndex].nextProduct} alt="Próximo produto" className="w-32 -translate-y-4 cursor-pointer" onClick={() => handleNavigate('next')} />
+							<img ref={potatoes2} src="/images/batatas2.png" alt="" className="w-40 absolute -left-16 top-16" />
 						</div>
 					</div>
 				</div>
-            )}
-           
-            {/* Navigation Buttons */}
-            <div className="absolute bottom-10 left-1/2 -translate-x-1/2 z-30 flex gap-4 items-center justify-between p-4">
-                <button
-                    onClick={() => handleNavigate('prev')}
-                    disabled={isAnimating}
-                    className="bg-black/30 text-white rounded-full p-2 hover:bg-black/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-                    aria-label="Anterior"
-                >
-                    <ArrowLeft size={16} />
-                </button>
 
-                <button
-                    onClick={() => handleNavigate('next')}
-                    disabled={isAnimating}
-                    className="bg-black/30 text-white rounded-full p-2 hover:bg-black/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-                    aria-label="Próximo"
-                >
-                    <ArrowRight size={16} />
-                </button>
-            </div>
-        </div>
+				{/* The animating slide is rendered on top only when the animation is in progress */}
+				{isAnimating && (
+					<div
+						key={animatingToIndex}
+						className={`absolute inset-0 ${slides[animatingToIndex].color} z-20 select-none`}
+						style={clipPathStyle}
+					>
+						<img src="/images/pringles.svg" alt="Pringles" className="-rotate-10 absolute -bottom-64 -right-48 w-160 -z-10 opacity-10" />
+
+						<div className="flex justify-between gap-32 p-24 pl-0 items-center">
+							<div className="w-60 select-none">
+								<div className="w-120">
+									<img ref={slides[animatingToIndex].refs.image} src={slides[animatingToIndex].image} alt="" className="rounded-full w-100 h-100 -translate-x-1/2 object-cover shadow-lg" />
+								</div>
+							</div>
+
+							<div className="flex flex-col gap-12 flex-1">
+								<h1 ref={slides[animatingToIndex].refs.title} className="font-black text-5xl">{slides[animatingToIndex].title}</h1>
+								<p ref={slides[animatingToIndex].refs.details} className="leading-relaxed text-sm w-120">{slides[animatingToIndex].details}</p>
+								<button ref={slides[animatingToIndex].refs.cta} className="uppercase cursor-pointer hover:opacity-90 transition-opacity p-3 bg-white rounded-md text-slate-800 font-medium w-fit">Ver na loja</button>
+							</div>
+
+							<div className="flex gap-6 items-end select-none w-[294px] relative">
+								<img ref={slides[animatingToIndex].refs.product} src={slides[animatingToIndex].product} alt="Produto" className="w-40" />
+								<img ref={slides[animatingToIndex].refs.nextProduct} src={slides[animatingToIndex].product} alt="Próximo produto" className="w-32" />
+								<img ref={slides[animatingToIndex].refs.lastProduct} src={slides[animatingToIndex].nextProduct} alt="Próximo produto" className="w-28 opacity-1" />
+								<img ref={potatoes} src="/images/batatas2.png" alt="" className="w-40 absolute -left-16 top-16" />
+							</div>
+						</div>
+					</div>
+				)}
+			
+				{/* Navigation Buttons */}
+				<div className="absolute bottom-10 left-1/2 -translate-x-1/2 z-30 flex gap-4 items-center justify-between p-4">
+					<button
+						onClick={() => handleNavigate('prev')}
+						disabled={isAnimating}
+						className="bg-black/30 text-white rounded-full p-2 hover:bg-black/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+						aria-label="Anterior"
+					>
+						<ArrowLeft size={16} />
+					</button>
+
+					<button
+						onClick={() => handleNavigate('next')}
+						disabled={isAnimating}
+						className="bg-black/30 text-white rounded-full p-2 hover:bg-black/50 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+						aria-label="Próximo"
+					>
+						<ArrowRight size={16} />
+					</button>
+				</div>
+			</div>
+
+			<div ref={triggerScroll}></div>
+		</main>
     )
 }
 
